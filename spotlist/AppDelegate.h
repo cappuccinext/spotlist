aa84f5fe5b32d4999cf8afd13e03e301d987e0da
cappuccinext <cappuccinext@MacBookProHaswell.local>
2014-06-28 14:38:53 +0900

    Initial Commit

---
+++ spotlist/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  spotlist
+//
+//  Created by cappuccinext on 2014/06/28.
+//  Copyright (c) 2014年 cappmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ spotlist/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  spotlist
+//
+//  Created by cappuccinext on 2014/06/28.
+//  Copyright (c) 2014年 cappmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
